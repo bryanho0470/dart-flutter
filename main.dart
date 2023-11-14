bc0748fb9d5559ac65bcf41d0e1decaeb30407d9
bryanho0470 <bryanho0470@gmail.com>
2023-11-14 06:00:09 +0200

    Maps

--- main.dart
+++ main.dart
@@ -1,10 +1,16 @@
 void main() {
-  var oldFriends = ['nice', 'lynn'];
-  var newFriends = [
-    'lewis',
-    'Park',
-    'darren',
-    for (var friend in oldFriends) "🥰 $friend"
-  ];
-  print(newFriends);
+  var player = {
+    'name': 'nico',
+    'xp': 19.99,
+    'superpower': false,
+  };
+  Map<int, bool> player2 = {
+    1: false,
+    2: true,
+    3: false,
+  };
+  Map<List<int>, bool> player3 = {
+    [1, 2, 3]: true,
+    [2, 4, 3]: false,
+  };
 }
