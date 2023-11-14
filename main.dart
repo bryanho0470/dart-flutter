942a67cf26b8f1445e564115fa66cfcbfe65658c
bryanho0470 <bryanho0470@gmail.com>
2023-11-14 06:14:52 +0200

    Set

--- main.dart
+++ main.dart
@@ -1,16 +1,10 @@
 void main() {
-  var player = {
-    'name': 'nico',
-    'xp': 19.99,
-    'superpower': false,
-  };
-  Map<int, bool> player2 = {
-    1: false,
-    2: true,
-    3: false,
-  };
-  Map<List<int>, bool> player3 = {
-    [1, 2, 3]: true,
-    [2, 4, 3]: false,
-  };
+  Set<String> player = {'nico', 'park'};
+  // var player = {'nico', 'park'}; same
+  player.add("park");
+  player.add("park");
+  player.add("park");
+  player.add("park");
+  player.add("park");
+  print(player);
 }
