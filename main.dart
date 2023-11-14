6dd7dfc9fb47fc391ed23d33f4d08548e876f359
bryanho0470 <bryanho0470@gmail.com>
2023-11-14 05:34:01 +0200

    dart variables

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "nico";
+  bool adult = true;
+  int age = 18;
+  double tall = 185.45;
+  final sex = "male";
+  const nationality = "south korea";
+  late final residence = "";
+}
