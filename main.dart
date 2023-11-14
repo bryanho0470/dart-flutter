ff0a61af635e8cf53c1bdf83716466f027167c25
bryanho0470 <bryanho0470@gmail.com>
2023-11-14 05:44:59 +0200

    how to do String interpolation

--- main.dart
+++ main.dart
@@ -1,12 +1,7 @@
 void main() {
-  var giveMeFive = true;
-  // List<int> number = [1,2,3,4.] same expression
-  var number = [
-    1,
-    2,
-    3,
-    4,
-    if (giveMeFive) 5,
-  ];
-  print(number);
+  var name = "Park";
+  var age = 42;
+  var greeting =
+      'Hello this is $name, Nice to meet you. I\'m ${age - 2} years old';
+  print(greeting);
 }
