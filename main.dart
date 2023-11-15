2136fd343d88384681dce824ac8819805211b583
bryanho0470 <bryanho0470@gmail.com>
2023-11-15 06:06:28 +0200

    optional positional parameter null safety

--- main.dart
+++ main.dart
@@ -1,11 +1,8 @@
-String sayHello({
-  required String name,
-  required int age,
-  required String country,
-}) {
+String sayHello(String name, int age, [String? country]) {
   return ("hello $name, you are $age years old. and you came from $country!!");
 }
 
 void main() {
-  print(sayHello(age: 40, country: 'cuba', name: 'Park'));
+  var result = sayHello('cuba', 40);
+  print(result);
 }
