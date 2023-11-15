9431e9d40ea752560d3ee2f1011b86e23941345c
bryanho0470 <bryanho0470@gmail.com>
2023-11-15 06:16:42 +0200

    QQ operator

--- main.dart
+++ main.dart
@@ -1,8 +1,9 @@
-String sayHello(String name, int age, [String? country]) {
-  return ("hello $name, you are $age years old. and you came from $country!!");
-}
+String capitalizeName(String? name) =>
+  // name != null ? name.toUpperCase() : 'ANON';
+  name?.toUpperCase() ?? 'ANON';
+
+
 
 void main() {
-  var result = sayHello('cuba', 40);
-  print(result);
+  capitalizeName('nico')
 }
