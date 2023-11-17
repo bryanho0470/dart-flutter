f5b0a1b700e15a8d8d81d30cbb597b4b1d88e1e9
bryanho0470 <bryanho0470@gmail.com>
2023-11-17 03:46:37 +0200

    named constructor practice

--- main.dart
+++ main.dart
@@ -1,51 +1,38 @@
 class Player {
   final String name, team;
-  int xp, age;
-
-  // constructor
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-    required this.age,
-  });
-
-  Player.createRedPlayer({required String name, required int age})
-      : this.name = name,
-        this.age = age,
-        this.team = "red",
-        this.xp = 0;
-
-  Player.createBluePlayer(String name, int age)
-      : this.name = name,
-        this.age = age,
-        this.team = "blue",
-        this.xp = 10;
+  int xp;
 
   void sayHello() {
     print(
-        "say hello $name I'm $age years old. I'm in $team team. I have experience for $xp years."); // dont need {this.name}
+        "say hello $name I'm in $team team. I have experience for $xp years."); // dont need {this.name}
   }
+
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        team = playerJson['team'],
+        xp = playerJson['xp'];
 }
 
 void main() {
-  var player = Player(
-    name: "nico",
-    xp: 1500,
-    team: "cuba",
-    age: 14,
-  ); //new Player() is also possible
-  player.sayHello();
-
-  var redPlayer = Player.createRedPlayer(
-    name: "hoho",
-    age: 20,
-  );
-  redPlayer.sayHello();
-
-  var bluePlayer = Player.createBluePlayer(
-    "BUBU",
-    21,
-  );
-  bluePlayer.sayHello();
+  var apiData = [
+    {
+      "name": "nico",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "park",
+      "team": "green",
+      "xp": 10,
+    },
+    {
+      "name": "lynn",
+      "team": "bule",
+      "xp": 20,
+    },
+  ];
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
 }
